c5da724f9d288bbd68fe8d0ff4c53ad01947fe71
unknown <sebastien.servouze@gmail.com>
2024-10-20 20:28:24 +0200

    :recycle: Correction du scan des entités

--- src/db/Datasource.ts
+++ src/db/Datasource.ts
@@ -6,6 +6,13 @@ if (dotenv.config().error) {
     logger.warn('No environment variables found. Using SQLite in-mem database.')
 }
 
+let entities;
+if (process.env.NODE_ENV === 'prod') {
+    entities = [__dirname + '../../**/*.entity.js'];
+} else {
+    entities = [__dirname + '../../**/*.entity.ts'];
+}
+
 const options: DataSourceOptions = {
     type: (process.env.DB_TYPE ?? 'sqlite') as any,
     host: process.env.DB_HOST ?? undefined,
@@ -15,8 +22,10 @@ const options: DataSourceOptions = {
     database: process.env.DB_NAME ?? 'sqlite.db',
     synchronize: false,
     logging: false,
-    entities: [__dirname + '../../**/*.entity.{js,ts}'],
+    entities: entities,
     migrations: [__dirname + '/migrations/*.{js,ts}'],
 }
+
+logger.debug(`Datasource options: ${JSON.stringify(options)}`);
 
 export const Datasource = new DataSource(options);
